7ac2c7904bb91e33168d39c8590b6e2aa566e7df
GuiPoloniato <guilhermeguipoloniato@gmail.com>
2024-05-22 11:51:03 -0300

    style: CSS da tela de login

--- src/pages/_login/login.jsx
+++ src/pages/_login/login.jsx
@@ -5,8 +5,15 @@ export default function Login() {
 
     return(
         <div className="body">
-            <h2>Aqui esta o login</h2>
-            <input type="email" name="email" id="email" placeholder="Seu email"/>
+            <div className="box">
+                <div className="form">
+                    <img src="/logo.png" alt="psicare" />
+                    <input type="email" name="email" id="email" placeholder="Seu email"/>
+                    <input type="password" name="senha" id="senha" placeholder="Sua senha"/>
+                    <button type="submit">Entrar</button>
+                </div>
+                
+            </div>
         </div>
     )
 }
--- src/styles/style.jsx
+++ src/styles/style.jsx
@@ -4,6 +4,6 @@ export const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
-    backgroundcolor: black;
+    font-family: sans-serif;
 }
 `
