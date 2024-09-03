937904922f4c5f910f2d9b314c215d20be9e53cd
GuiPoloniato <guilhermeguipoloniato@gmail.com>
2024-09-02 22:43:37 -0300

    fix: style arrumado

--- src/components/visualizar/paciente.jsx
+++ src/components/visualizar/paciente.jsx
@@ -1,22 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import TableProfAluno from "../table/profAlunos";
 import "./style.css";
 
 export default function VisualizarPaciente({handleCloseVisualizar, dadosPaciente}){
     const alunosPaciente = dadosPaciente._id;
-
-    return(
-        <div className="visualizar">
-            <div className="body-visualizar">
-                <div className="header-visualizar">
-                    <div className="header-visualizar-info">
-                        <h1>Informações sobre professor</h1>
-=======
-import "./style.css";
-
-
-export default function VisualizarPaciente({handleCloseVisualizar, dadosPaciente}){
 
     const formatarDataNascimento = (data) => {
         const dataObj = new Date(data);
@@ -31,47 +17,12 @@ export default function VisualizarPaciente({handleCloseVisualizar, dadosPaciente
                 <div className="header-visualizar header-visualizar-paciente">
                     <div className="header-visualizar-info">
                         <h1>Informações sobre paciente</h1>
->>>>>>> a54e0087bbc541a3f126da3a1eb74dee6d28f94f
                         <div className="voltar">
                             <button className="button-voltar" onClick={handleCloseVisualizar} >Voltar</button>
                         </div>
                     </div>
-                    <hr />     
-                </div>
-<<<<<<< HEAD
-                <div className="visualizar-info">
-                    <div className="coluna1">
-                        <div className="nome">
-                            <p>Nome</p>
-                            <h1>{dadosPaciente.nome}</h1>
-                        </div>
-                    </div>
-                    <div className="coluna2">
-                        <div className="email">
-                            <p>Email</p>
-                            <h1>{dadosPaciente.email}</h1>
-                        </div>
-                        <div className="telefone">
-                            <p>Telefone</p>
-                            <h1>{dadosPaciente.telefone}</h1>
-                        </div>
-                    </div>  
-                    <div className="coluna3">
-                        <div className="disciplina">
-                            <p>Disciplina</p>
-                            <h1>{dadosPaciente.disciplina}</h1>
-                        </div>
-                        <div className="cpf">
-                            <p>CPF</p>
-                            <h1>{dadosPaciente.cpf}</h1>
-                        </div>
-                    </div>   
-                </div>     
-                
-            </div>
-            {/* <TableProfAluno alunosPaciente={alunosPaciente}/> */}
-=======
-                <div className="visualizar-info visualizar-info-paciente">
+                    <hr />  
+                    <div className="visualizar-info visualizar-info-paciente">
                     <h2>Informações Pessoais</h2>
                     <div className="coluna1">
                             <div className="nome">
@@ -189,10 +140,9 @@ export default function VisualizarPaciente({handleCloseVisualizar, dadosPaciente
                                 <h1>{dadosPaciente.tipoDeTratamento}</h1>
                             </div> 
                         </div>
-                    </div>     
-                
+                    </div>   
+                </div>  
             </div>
->>>>>>> a54e0087bbc541a3f126da3a1eb74dee6d28f94f
         </div>
     )
 }
